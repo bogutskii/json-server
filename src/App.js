d234f5f695576f41dc05e77bea66f52bf924a05c
bogutskii <sl9site@gmail.com>
2024-10-15 21:48:53 -0700

    add info to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Database Testing Page</h1>
+          <p>
+            This page is created for testing a database using the <strong>JSON Server</strong> technology.
+          </p>
+          <p>
+            You can access the user data through the following endpoint:
+          </p>
+          <a
+              className="App-link"
+              href="http://localhost:3001/users"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            http://localhost:3001/users
+          </a>
+          <p>
+            The purpose of this page is to allow testing and interaction with the JSON-based database. You can perform GET, POST, PUT, and DELETE requests to this endpoint.
+          </p>
+        </header>
+      </div>
   );
 }
 
